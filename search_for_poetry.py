62da4429c814ec4b85399b8f4d2ce0d835dc046e
SCU-ChenYue <46099189+SCU-ChenYue@users.noreply.github.com>
2021-12-28 20:39:25 +0800

    Add files via upload

---
+++ search_for_poetry.py
@@ -0,0 +1,392 @@
+# coding:utf-8
+from gensim import corpora
+from gensim.models import TfidfModel
+from model_TFIDF_LDA import get_stopwords
+from nltk.corpus import wordnet as wn
+from gensim.summarization import bm25
+import jieba
+import csv
+from gensim.models import KeyedVectors
+import jieba.posseg as psg
+from ltp import LTP
+import jieba.analyse
+import functools
+import synonyms
+from util.utils import normalization, compare_right, compare_left, cut_sent
+
+# 可接受词性 jieba
+# flags = ['a', 'j', 'n', 'vn', 'ns', 't', 'v', 's', 'ad', 'ag', 'an', 'ng', 'tg', 'vg', 'vd', 's', 'i', 'l']
+# 停止词性 LTP
+flags = ['c', 'e', 'm', 'nh', 'o', 'p', 'r', 'u', 'wp', 'ws', 'x']
+corpus_path = "去重之后/总的去重合集/诗级别的格式化数据（赏析分段+赏析去重+关键词LTP）.csv"  # ['编号', '标题', '作者', '朝代', '原文', '翻译', '注释', '赏析', '标签']
+embedding_path = "wordEmbedding/sgns.baidubaike.bigram-char"
+tfidfPath = "checkpoint/checkpoint_9/poetry_tfidf.model"
+dicpath = "checkpoint/checkpoint_9/poetry_dic.dict"
+allTagpath = "去重之后/分别提取的数据/tag.txt"
+stop_path = "stopwords.txt"
+stopwords = get_stopwords(stop_path)
+tfidfModel = TfidfModel.load(tfidfPath)
+dictionary = corpora.Dictionary.load(dicpath)
+model = KeyedVectors.load_word2vec_format(embedding_path, binary=False)
+words = model.vocab
+words_list = [word for i, word in enumerate(words)]  # 词向量文件中的词列表
+ltp = LTP(path="small")
+
+
+def get_all_tags():
+    tag_list = []
+    f = open(allTagpath, "r", encoding="utf-8")
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip().replace("\n", "")
+        if line != "":
+            tag_list.append(line)
+    tag_list = list(set(tag_list))
+    return tag_list
+
+
+tagList = get_all_tags()    # 所有的诗歌标签
+
+
+def get_corpus(path):
+    corpus = []
+    poetry_list = []
+    f = csv.reader(open(path, 'r', encoding='utf-8'))
+    for i in f:
+        if len(i) == 0 or i[0] == '编号':
+            continue
+        tags = i[8].split("，")
+        corpus.append(tags)
+        poetry_list.append(i)
+    bm25Model = bm25.BM25(corpus)
+    return bm25Model, poetry_list
+
+
+def tfidf_normalization(qTFIDF):  # 对query的tifdf结果进行归一化
+    scores = []
+    tfidf_state = []
+    for item in qTFIDF:
+        scores.append(item[1])
+    scores = normalization(scores)
+    for i in range(len(qTFIDF)):
+        tfidf_state.append((qTFIDF[i][0], scores[i]))
+    return tfidf_state
+
+
+def wash_analyze(clist):
+    analyze_list = []
+    newcontent = ""
+    for c in clist:  # 每一个段落
+        paragraph = ""
+        senList = cut_sent(c)
+        for sen in senList:
+            if "《" not in sen and "》" not in sen:
+                paragraph += sen
+                newcontent += sen
+        analyze_list.append(paragraph)
+    return analyze_list, newcontent    # 由每个段落组成的list [...,...]
+
+
+def cut_query(context, q, k=5):  # 原输入句子和去停用词后的词语列表
+    token_score = {}
+    for item in q:
+        token_score[item] = 0
+
+    # 先使用语料库中的tfidf进行打分
+    corpus = [dictionary.doc2bow(q)]
+    corpus_tfidf = list(tfidfModel[corpus])[0]
+    corpus_tfidf.sort(key=functools.cmp_to_key(compare_right))
+    token_state = []
+    for item in corpus_tfidf:
+        word = dictionary[item[0]]
+        token_state.append((word, item[1]))
+    print("语料库的tf-idf打分情况：", token_state)
+
+    for item in token_state:
+        token_score[item[0]] = item[1]
+    print("被补充前的token_score：", token_score)
+    extra_words = []
+    for word, score in token_score.items():
+        if score == 0:
+            extra_words.append(word)
+    if len(extra_words) == 0:
+        return
+
+    # 再用自带的tf-idf和text-rank打分，用于补充
+    jieba.analyse.set_stop_words(stop_path)
+    # qr = jieba.analyse.textrank(context, topK=20, withWeight=True,
+    #                             allowPOS=('a', 'j', 'n', 'vn', 'ns', 't', 'v', 's', 'ad', 'ag', 'an',
+    #                                       'ng', 'tg', 'vg', 'vd', 's', 'i'))
+    # print("text-rank重要性解析结果：", qr)
+    qt = jieba.analyse.extract_tags(context, topK=20, withWeight=True,
+                                    allowPOS=('a', 'j', 'n', 'vn', 'ns', 't', 'v', 's', 'ad', 'ag', 'an',
+                                              'ng', 'tg', 'vg', 'vd', 's', 'i'))
+    print("归一化前的tf-idf重要性解析结果：", qt)
+    if len(qt) != 0:
+        qt = tfidf_normalization(qt)
+    print("归一化后的tf-idf重要性解析结果：", qt)
+
+    for word in extra_words:
+        for item in qt:
+            if word == item[0]:
+                token_score[word] = item[1]
+                break
+    token_score = sorted(token_score.items(), key=lambda d: d[1], reverse=True)
+    print("被补充、排序后的token_score：", token_score)
+    sorted_tokens = []
+    for word, score in token_score[:k]:
+        sorted_tokens.append(word)
+    return sorted_tokens
+
+
+def get_result(qs, aidf):
+    scores = BM25Model.get_scores(qs, aidf)  # 输入的文本内容必须是由关键词组成的列表
+    paragraph_score = []
+    for i in range(len(scores)):    # 分数，标题，作者，朝代，原文，翻译，注释，赏析，标签
+        paragraph_score.append((scores[i], poetryList[i][1], poetryList[i][2], poetryList[i][3], poetryList[i][4],
+                                poetryList[i][5], poetryList[i][6], poetryList[i][7], poetryList[i][8]))
+    paragraph_score.sort(key=functools.cmp_to_key(compare_left))
+    for item in paragraph_score[:25]:
+        if item[0] == 0:
+            break
+        print(item[0], " ", item[1], " ", item[2], " ", item[3], " ", item[4])
+    return paragraph_score
+
+
+def search_multiSentence_analyze(contents, translations, analyze_list):
+    sentence_analyze = []
+    for i in range(len(contents)):  # 若原文和译文对齐，则依次遍历每一句话。
+        sentence = contents[i]
+        if len(translations) == 0:  # 有可能并没有传进来翻译
+            trans = ""
+        else:
+            trans = translations[i]
+        corpus = []
+
+        for paragraph in analyze_list:  # 遍历赏析的每一个文段
+            seg, hidden = ltp.seg([paragraph])
+            seg = seg[0]
+            pos = ltp.pos(hidden)[0]
+            wlist = []
+            for k in range(len(seg)):
+                word, flag = seg[k], pos[k]
+                wlist.append(word)
+            corpus.append(wlist)
+        bm25Model = bm25.BM25(corpus)
+
+        aidf = sum(map(lambda m: float(bm25Model.idf[k]), bm25Model.idf.keys())) / len(bm25Model.idf.keys())
+        poetry_query = ltp.seg([sentence + trans])
+        scores = bm25Model.get_scores(poetry_query, aidf)  # 搜索得到该句话对应每个文段的分数
+        paragraphs_score = []
+
+        for j in range(len(scores)):
+            paragraphs_score.append((scores[j], analyze_list[j]))
+        paragraphs_score.sort(key=functools.cmp_to_key(compare_left))
+        if paragraphs_score[0][0] <= paragraphs_score[1][0] * 3:  # 如果第一、二段的分数在三倍以内，则两段都要
+            paragraph = paragraphs_score[0][1] + paragraphs_score[1][1]
+        else:
+            paragraph = paragraphs_score[0][1]
+        sentence_analyze.append((sentence, trans, paragraph))
+    return sentence_analyze
+
+
+def sentence_fromPoetry(poetry_item, qs):  # 对于一首诗，利用原文、译文，从赏析中检索对应的文段。
+    content, translation, analyze = poetry_item[4], poetry_item[5], poetry_item[7]
+    content, translation, analyze = content.replace(" ", ""), translation.replace(" ", ""), analyze.replace(" ", "")
+    # 对赏析进行分段，并再分成短句后进行清洗。
+    analyzes = analyze.split("|")
+    analyze_list, _ = wash_analyze(analyzes)
+    if "\n" in analyzes:
+        analyzes.remove("\n")
+    if "" in analyzes:
+        analyzes.remove("")
+    contents, translations = content.split("|"), translation.split("|")
+    if "" in contents:
+        contents.remove("")
+    if "" in translations:
+        translations.remove("")
+
+    if len(contents) == 1:  # 如果默认分句的结果只有一句
+        poetry_contents = content.split("。")    # 尝试手动分句
+        trans_contents = translation.split("。")
+        if len(poetry_contents) == 1 or len(poetry_contents) != len(trans_contents):  # 若分完还是一句或没法对齐。
+            return [(content, translation, analyze)]
+        else:   # 若手动分完后是对齐的了...
+            sentenceAnalyzes = search_multiSentence_analyze(poetry_contents, trans_contents, analyze_list)
+    elif len(contents) == len(translations):    # 本来就对齐
+        sentenceAnalyzes = search_multiSentence_analyze(contents, translations, analyze_list)
+    else:   # 若不对齐
+        poetry_contents = content.split("。")    # 先尝试手动分句
+        trans_contents = translation.split("。")
+        if len(poetry_contents) == 1:    # 若手动分句后只有一句
+            return [(content, translation, analyze)]
+        elif len(poetry_contents) != len(trans_contents):   # 若手动分句后有多句，但是不对齐，则只用原文去搜
+            sentenceAnalyzes = search_multiSentence_analyze(poetry_contents, [], analyze_list)
+
+
+
+def expand_query(q_list, k=4):   # 进行扩充
+    print("进行扩充，每个词扩充", k, "个。")
+    query_list = q_list.copy()
+    for q in q_list:
+        wordnets = []
+        print("开始wordnet扩充。")
+        wordnet_expansion = []
+        wordnetSynset = wn.synsets(q, lang='cmn')
+        for synset in wordnetSynset:
+            wordnets.extend(synset.lemma_names('cmn'))
+        wordnets = list(set(wordnets))
+        words = []
+        for token in wordnets:
+            token = token.replace("+", "")
+            words.append(token)
+        wordnets = words    # 清除了+号之后，再赋值回给原来的
+        Kcount = 0
+        for i in range(len(wordnets)):
+            if Kcount == k:
+                break
+            if wordnets[i] in tagList and wordnets[i] not in query_list:
+                Kcount += 1
+                print("wordnet扩充了：", wordnets[i])
+                wordnet_expansion.append(wordnets[i])
+        if len(wordnet_expansion) > 1:
+            wordnet_expansion.append(q)
+            different_token = model.doesnt_match(wordnet_expansion)
+            if different_token != q:
+                print("wordnet处筛掉：", different_token)
+                wordnet_expansion.remove(different_token)
+                Kcount -= 1
+            else:
+                print("wordnet处不删掉。")
+        query_list.extend(wordnet_expansion)
+        kkcount = k - Kcount  # 还差多少个
+        if kkcount <= 0:
+            continue
+        print("开始synonyms扩充。")
+        synonyms_expansion = []
+        synonyms_list = synonyms.nearby(q, 100)
+        synonyms_words = synonyms_list[0]
+        synonyms_scores = synonyms_list[1]
+        for i in range(len(synonyms_words)):
+            word = synonyms_words[i]
+            score = synonyms_scores[i]
+            if kkcount == 0 or score < 0.5:  # synonyms的结果不能全要，要根据分数来进一步选择。
+                break
+            if word not in query_list and word in tagList:
+                print("synonyms扩充了：", word)
+                synonyms_expansion.append(word)
+                kkcount -= 1
+        if len(synonyms_expansion) > 1:
+            synonyms_expansion.append(q)
+            different_token = model.doesnt_match(synonyms_expansion)
+            if different_token != q:
+                synonyms_expansion.remove(different_token)
+                print("synonyms处筛掉：", different_token)
+                kkcount += 1
+            else:
+                print("synonyms处不删掉。")
+        query_list.extend(synonyms_expansion)
+        if kkcount <= 0:
+            continue
+        print("开始word2vec扩充。")
+        word2vec_expansion = []
+        result_list = model.most_similar(q, topn=100)
+        for item in result_list:
+            if kkcount == 0 or item[1] < 0.5:
+                break
+            if item[0] not in query_list and item[0] in tagList:
+                print("word2vec扩充了：", item[0])
+                word2vec_expansion.append(item[0])
+                kkcount -= 1
+        if len(word2vec_expansion) > 1:
+            word2vec_expansion.append(q)
+            different_token = model.doesnt_match(word2vec_expansion)
+            if different_token != q:
+                print("word2vec处筛掉：", different_token)
+                word2vec_expansion.remove(different_token)
+                kkcount += 1
+            else:
+                print("word2vec处不删掉。")
+        query_list.extend(word2vec_expansion)
+
+    query_list = list(set(query_list))
+    print("扩充后：", query_list)
+    return query_list
+
+
+def replace_synonyms(qlist):    # 遍历分词后的word列表，并将不在数据集的词表中的词语换成数据集中的近义词
+    queryList = []
+    for token in qlist:
+        if token not in tagList:
+            print("需要替换：", token)
+            count = 0
+            if token not in words_list:
+                print("无法替换：", token)
+                continue
+            print("正在通过word2vec获取100个近义词。")
+            result_list = model.most_similar(token, topn=100)
+            print("获取完成。")
+            for item in result_list:
+                if count == 3:
+                    break
+                if item[0] in tagList:
+                    print("替换结果：", item[0])
+                    queryList.append(item[0])
+                    count += 1
+                    # break
+        else:
+            queryList.append(token)
+    return queryList
+
+
+def process_query(context):
+    seg, hidden = ltp.seg([context])
+    seg = seg[0]
+    pos = ltp.pos(hidden)[0]
+    qs = []
+    for i in range(len(seg)):
+        word = seg[i].strip()
+        flag = pos[i].strip()
+        if word not in stopwords and flag not in flags:
+            qs.append(word)
+    # query = psg.cut(context)
+    # qs = []
+    # for word, flag in query:
+    #     if word not in stopwords and flag in flags:
+    #         qs.append(word)
+    qs = list(set(qs))
+    print("去掉停用词和停用词性后的结果：", qs)
+    qs = replace_synonyms(qs)   # 替换不在词表中的词
+    print("进行近义词替换后的结果：", qs)
+    if len(qs) <= 2:    # 若此时词语很少
+        # 进行词语扩充
+        qs = expand_query(qs, k=3)
+
+    # if len(qs) > 5:
+    #     qs = cut_query(context, qs, k=5)
+
+    print("初步检索的结果：")
+    paragraph_score = get_result(qs, average_idf)
+
+    # 自动扩充搜索范围
+    if len(paragraph_score) == 0 or paragraph_score[0][0] <= 8:  # 若没有获取到结果，或者分数都不高
+        if len(qs) > 5:  # 如果关键词太多，则需要取重要的
+            # qs = cut_query(context, qs, k=5)
+            qs = expand_query(qs, 3)
+        elif len(qs) == 1:
+            qs = expand_query(qs, 6)
+        else:
+            qs = expand_query(qs, 4)
+        print("扩充后再次检索：")
+        new_paragraph_score = get_result(qs, average_idf)
+
+
+if __name__ == '__main__':
+    BM25Model, poetryList = get_corpus(corpus_path)
+    average_idf = sum(map(lambda k: float(BM25Model.idf[k]), BM25Model.idf.keys())) / len(BM25Model.idf.keys())
+    print("请开始输入，输入quit结束。")
+    query = input()
+    while query != "quit":
+        process_query(query)
+        print("请继续输入，输入quit结束：")
+        query = input()
---
+++ utils.py
@@ -0,0 +1,144 @@
+# coding:utf-8
+import requests
+from util.langconv import *
+import jieba.posseg as psg
+import numpy as np
+from ltp import LTP
+import re
+
+
+# 请求html文件
+def getHTMLText(url):
+    try:
+        kv = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(url, headers=kv)  # 防止查源user-agent
+        r.raise_for_status()  # 确保200
+        r.encoding = r.apparent_encoding  # 防止乱码
+        demo = r.text
+        return demo
+    except:
+        print("1爬取失败")
+
+
+# 转换繁体到简体
+def cht_to_chs(line):
+    line = Converter('zh-hans').convert(line)
+    line.encode('utf-8')
+    return line
+
+
+# 转换简体到繁体
+def chs_to_cht(line):
+    line = Converter('zh-hant').convert(line)
+    line.encode('utf-8')
+    return line
+
+
+# 自定义排序方式
+def compare(a1, a2):
+    if a1['score'] < a2['score']:
+        return 1
+    elif a1['score'] > a2['score']:
+        return -1
+    return 0
+
+
+def compare_right(a1, a2):
+    if a1[1] < a2[1]:
+        return 1
+    elif a1[1] > a2[1]:
+        return -1
+    return 0
+
+
+def compare_left(a1, a2):
+    if a1[0] < a2[0]:
+        return 1
+    elif a1[0] > a2[0]:
+        return -1
+    return 0
+
+
+# 清洗中文文本的各项字符
+def wash_content(token):
+    bad_token = [" ", "\n", "\r", "\u3000", ".", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "…", "	",
+                 "▲", "~"]
+    for item in bad_token:
+        token = token.replace(item, "")
+    return token
+
+
+def cut_sent(para):  # 分句
+    para = re.sub('([。！；？\?])([^”’])', r"\1\n\2", para)  # 单字符断句符
+    para = re.sub('(\.{6})([^”’])', r"\1\n\2", para)  # 英文省略号
+    para = re.sub('(\…{2})([^”’])', r"\1\n\2", para)  # 中文省略号
+    para = re.sub('([。！；？\?][”’])([^，。！？\?])', r'\1\n\2', para)
+    # 如果双引号前有终止符，那么双引号才是句子的终点，把分句符\n放到双引号后，注意前面的几句都小心保留了双引号
+    para = para.rstrip()  # 段尾如果有多余的\n就去掉它
+    # 很多规则中会考虑分号;，但是这里我把它忽略不计，破折号、英文双引号等同样忽略，需要的再做些简单调整即可。
+    return para.split("\n")
+
+
+def cut_sent_mini(para):
+    para = re.sub('([。！，：；”“"\….？\?])', "|", para)
+    para = para.rstrip()
+    return para.split("|")
+
+
+def get_stopwords(path):
+    stopwords = []
+    f = open(path, 'r', encoding='utf-8')
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        if line != "":
+            stopwords.append(line)
+    return stopwords
+
+
+# 对文本分词（去除停用词和停用词性） jieba
+def tokenizer(text):
+    document = []
+    word_flags = []
+    text = wash_content(text)
+    line = re.sub(r"[\w\d]+", " ", text, flags=re.ASCII)
+    # 停用词性：n名词 nr人名 v动词 ns地名 nt机构团体名 d副词
+    flags = ['a', 'j', 'n', 'vn', 'ns', 't', 'v', 's', 'ad', 'ag', 'an', 'ng', 'tg', 'vg', 'vd', 's', 'i', 'l']
+    # flags = ['a', 'j', 'n', 'vn', 'ns', 't', 'v', 'i', 'z']  # 构建tag时用的词性列表
+    stop_words = get_stopwords('stopwords.txt')
+    for word, flag in psg.cut(line):
+        word = word.strip()
+        if flag in flags and word not in stop_words:
+            document.append(word)
+            word_flags.append(flag)
+    return document, word_flags
+
+
+def tokenizer_LTP(text, ltp):
+    document = []
+    word_flags = []
+    text = wash_content(text)
+    line = re.sub(r"[\w\d]+", " ", text, flags=re.ASCII)
+    flags = ['c', 'e', 'm', 'nh', 'o', 'p', 'r', 'u', 'wp', 'ws', 'x']
+    stop_words = get_stopwords('stopwords.txt')
+    seg, hidden = ltp.seg([line])
+    seg = seg[0]
+    pos = ltp.pos(hidden)[0]
+    for i in range(len(pos)):
+        word = seg[i].strip()
+        flag = pos[i]
+        if flag not in flags and word not in stop_words:
+            document.append(word)
+            word_flags.append(flag)
+    return document, word_flags
+
+
+def normalization(data):
+    _range = np.max(data) - np.min(data)
+    return (data - np.min(data)) / _range
+
+
+def standardization(data):
+    mu = np.mean(data, axis=0)
+    sigma = np.std(data, axis=0)
+    return (data - mu) / sigma
